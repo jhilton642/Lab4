08d255efd43f3a1d7847a27d8cc86757da955dbc
jhilton642 <joshua.hilton@att.net>
2020-09-17 09:49:42 -0500

    Add project files.

---
+++ Lab4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declarations
+            int nLargest;
+            int nSmallest;
+            int nCurrent;
+            string strEntry;
+            //first integer
+            strEntry = Utils.GetInput("Enter an integer ");
+            nLargest = nSmallest = Convert.ToInt32(strEntry);
+            //loop
+            while (strEntry != "Q")
+            {
+                nCurrent = Convert.ToInt32(strEntry);
+
+                //check to see if input should replace largest or smallest
+                if (nCurrent > nLargest)
+                {
+                    nLargest = nCurrent;
+                }
+                else if (nCurrent < nSmallest)
+                {
+                    nSmallest = nCurrent;
+                }
+                strEntry = Utils.GetInput("Enter another integer or Q to Quit ");
+            }
+            Console.WriteLine("The largest value you entered was " + nLargest + ".");
+            Console.WriteLine("The smallest value you entered was " + nSmallest + ".");
+        }
+    }
+}
